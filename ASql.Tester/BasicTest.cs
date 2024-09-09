cfbc81b9a99899d6601969653a556f96739d6bd8
Skimmenthal13 <vincenzo.schimmenti@gmail.com>
2024-09-09 14:31:09 +0200

    Added more target frameworks and downgraded Oracle Managed data access

--- ASql.Tester/BasicTest.cs
+++ ASql.Tester/BasicTest.cs
@@ -14,8 +14,8 @@ namespace ASql.Tester
         const string tableName = "PERSON";
         static byte[] _FileBytes = File.ReadAllBytes("./headshot.png");
 
-        [DataRow(ASqlManager.DBType.SqlServer, sqlConnectionString, $"CREATE TABLE {tableName} ([id] [int] IDENTITY(1,1) NOT NULL , [firstname] [nvarchar](30) NOT NULL , [lastname] [nvarchar](30) NOT NULL , [age] [int] NULL , [value] [bigint] NULL , [birthday] [datetime2] NULL , [hourly] [decimal](18,2) NULL , [localtime] [datetimeoffset] NULL , [picture] [varbinary](max) NULL , [guid] [varchar](36) NULL , [active] [tinyint] NULL , CONSTRAINT [PK_person] PRIMARY KEY CLUSTERED (  [id] ASC ) WITH (  PAD_INDEX = OFF,   STATISTICS_NORECOMPUTE = OFF,   IGNORE_DUP_KEY = OFF,   ALLOW_ROW_LOCKS = ON,   ALLOW_PAGE_LOCKS = ON ) ON [PRIMARY] ) ON [PRIMARY] ")]
-        [DataRow(ASqlManager.DBType.Oracle, oraConnectionString, $"CREATE TABLE {tableName} (id number(11) NOT NULL , firstname varchar2(30 char) NOT NULL , lastname varchar2(30 char) NOT NULL , age number(11) NULL , value number(12) NULL , birthday timestamp NULL , hourly decimal(18,2) NULL , localtime timestamp NULL , picture blob  NULL , guid varchar2(36) NULL , active number(37) NULL ,PRIMARY KEY (id))")]
+        [DataRow(ASqlManager.DBType.SqlServer, sqlConnectionString, $"CREATE TABLE {tableName} ([id] [int] IDENTITY(1,1) NOT NULL , [firstname] [nvarchar](30) NOT NULL , [lastname] [nvarchar](30) NOT NULL , [age] [int] NULL , [value] [bigint] NULL , [birthday] [datetime2] NULL , [hourly] [decimal](18,2) NULL , [picture] [varbinary](max) NULL , [guid] [varchar](36) NULL , [active] [tinyint] NULL , CONSTRAINT [PK_person] PRIMARY KEY CLUSTERED (  [id] ASC ) WITH (  PAD_INDEX = OFF,   STATISTICS_NORECOMPUTE = OFF,   IGNORE_DUP_KEY = OFF,   ALLOW_ROW_LOCKS = ON,   ALLOW_PAGE_LOCKS = ON ) ON [PRIMARY] ) ON [PRIMARY] ")]
+        [DataRow(ASqlManager.DBType.Oracle, oraConnectionString, $"CREATE TABLE {tableName} (id number(11) NOT NULL , firstname varchar2(30 char) NOT NULL , lastname varchar2(30 char) NOT NULL , age number(11) NULL , value number(12) NULL , birthday timestamp NULL , hourly decimal(18,2) NULL , picture blob  NULL , guid varchar2(36) NULL , active number(37) NULL ,PRIMARY KEY (id))")]
         [TestMethod]
         public void CreateTableTest(ASqlManager.DBType dBType, string ConnectionString, string sql)
         {
@@ -137,7 +137,7 @@ namespace ASql.Tester
                 ASqlCommand cmd = new ASqlCommand(sql, conn);
                 cmd.Transaction = trans;
                 i = cmd.ExecuteNonQuery();
-                trans.Rollback();
+                trans.Commit();
                 Assert.AreEqual(-1, i);
             }
         }
@@ -188,16 +188,16 @@ namespace ASql.Tester
                     }
                 }
 
-                Assert.AreEqual("first1", i.ToUpper());
+                Assert.AreEqual("FIRST1", i.ToUpper());
             }
         }
-        [DataRow(ASqlManager.DBType.SqlServer, sqlConnectionString, $"INSERT INTO {tableName} (firstname,lastname,age,value,birthday,hourly,localtime,picture,guid,active) VALUES (@firstname,@lastname,@age,@value,@birthday,@hourly,@localtime,@picture,@guid,@active)")]
-        [DataRow(ASqlManager.DBType.Oracle, oraConnectionString, $"INSERT INTO {tableName} (firstname,lastname,age,value,birthday,hourly,localtime,picture,guid,active) VALUES (:firstname,:lastname,:age,:value,:birthday,:hourly,:localtime,:picture,:guid,:active)")]
+        [DataRow(ASqlManager.DBType.SqlServer, sqlConnectionString, $"INSERT INTO {tableName} (firstname,lastname,age,value,birthday,hourly,picture,guid,active) VALUES (@firstname,@lastname,@age,@value,@birthday,@hourly,@picture,@guid,@active)")]
+        [DataRow(ASqlManager.DBType.Oracle, oraConnectionString, $"INSERT INTO {tableName} (firstname,lastname,age,value,birthday,hourly,picture,guid,active) VALUES (:firstname,:lastname,:age,:value,:birthday,:hourly,:picture,:guid,:active)")]
         [TestMethod]
         public void InsertRow(ASqlManager.DBType dBType, string ConnectionString, string sql) 
         {
             Dictionary<string, object> d = new Dictionary<string, object>();
-            for (int i = 1; i < 20; i++)
+            for (int i = 1; i < 2; i++)
             {
                 d.Add("firstname", "first" + i);
                 d.Add("lastname", "last" + i);
@@ -205,7 +205,6 @@ namespace ASql.Tester
                 d.Add("value", i * 1000);
                 d.Add("birthday", DateTime.Now);
                 d.Add("hourly", 123.456);
-                d.Add("localtime", new DateTimeOffset(2021, 4, 14, 01, 02, 03, new TimeSpan(7, 0, 0)));
                 d.Add("picture", _FileBytes);
                 d.Add("guid", Guid.NewGuid());
                 d.Add("active", (i % 2 > 0));
@@ -217,17 +216,6 @@ namespace ASql.Tester
                 conn.Open();
                 ASqlCommand cmd = new ASqlCommand(sql, conn);
                 string paramChar = "";
-                switch (ASqlManager.DataBaseType)
-                {
-                    case ASqlManager.DBType.SqlServer:
-                        paramChar = "";
-                        break;
-                    case ASqlManager.DBType.Oracle:
-                        paramChar = ":";
-                        break;
-                    default:
-                        throw new NotSupportedException();
-                }
                 List<ASqlParameter> apc = GetParametersFromkeyValuePairs(d,paramChar);
                 foreach (ASqlParameter param in apc)
                 {
@@ -238,8 +226,8 @@ namespace ASql.Tester
                 Assert.AreEqual(1, i);
             }
         }
-        [DataRow(ASqlManager.DBType.SqlServer, sqlConnectionString, $"INSERT INTO {tableName} (firstname,lastname,age,value,birthday,hourly,localtime,picture,guid,active) VALUES (@firstname,@lastname,@age,@value,@birthday,@hourly,@localtime,@picture,@guid,@active)")]
-        [DataRow(ASqlManager.DBType.Oracle, oraConnectionString, $"INSERT INTO {tableName} (firstname,lastname,age,value,birthday,hourly,localtime,picture,guid,active) VALUES (:firstname,:lastname,:age,:value,:birthday,:hourly,:localtime,:picture,:guid,:active)")]
+        [DataRow(ASqlManager.DBType.SqlServer, sqlConnectionString, $"INSERT INTO {tableName} (firstname,lastname,age,value,birthday,hourly,picture,guid,active) VALUES (@firstname,@lastname,@age,@value,@birthday,@hourly,@picture,@guid,@active)")]
+        [DataRow(ASqlManager.DBType.Oracle, oraConnectionString, $"INSERT INTO {tableName} (firstname,lastname,age,value,birthday,hourly,picture,guid,active) VALUES (:firstname,:lastname,:age,:value,:birthday,:hourly,:picture,:guid,:active)")]
         [TestMethod]
         public void InsertRowTransaction(ASqlManager.DBType dBType, string ConnectionString, string sql)
         {
@@ -252,7 +240,6 @@ namespace ASql.Tester
                 d.Add("value", i * 1000);
                 d.Add("birthday", DateTime.Now);
                 d.Add("hourly", 123.456);
-                d.Add("localtime", new DateTimeOffset(2021, 4, 14, 01, 02, 03, new TimeSpan(7, 0, 0)));
                 d.Add("picture", _FileBytes);
                 d.Add("guid", Guid.NewGuid());
                 d.Add("active", (i % 2 > 0));
@@ -285,7 +272,7 @@ namespace ASql.Tester
                 }
 
                 i = cmd.ExecuteNonQuery();
-                trans.Commit();
+                trans.Rollback();
                 Assert.AreEqual(1, i);
             }
         }
